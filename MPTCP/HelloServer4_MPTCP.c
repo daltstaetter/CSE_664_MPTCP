912140b5bdde931886c73666ffadac6a5e6886ab
Dalton Altstaetter <daltstaetter@gmail.com>
2019-04-08 18:16:33 -0500

    Add/Print options to confirm MPTCP_ENABLED
    Signed-off-by: Dalton Altstaetter <daltstaetter@gmail.com>

--- MPTCP/HelloServer4_MPTCP.c
+++ MPTCP/HelloServer4_MPTCP.c
@@ -11,11 +11,12 @@
 
 //#include <netinet/tcp.h> 	// SOL_TCP
 #include <linux/tcp.h> 	// MPTCP_* socket options
+//#include <net/mptcp.h>  // not sure if I should be explicitly adding this header, ADD_ADDR
 
 #include <arpa/inet.h> 		// inet_ntoa
 #include <time.h>
 
-#define ERROR 		-1
+#define ERROR		-1
 #define TCP_PORT 	10000
 #define TCP_IP 		"127.0.0.1"
 #define TCP_IP0_VM1	"192.168.56.102"
@@ -23,9 +24,9 @@
 #define BUFF_SIZE	1024
 
 //-----------------------------------------------------
-#define SOL_TCP              		6 // as defined in netinet/tcp.h
+#define SOL_TCP              			6 // as defined in netinet/tcp.h
 #define MPTCP_INFO_FLAG_SAVE_MASTER 	0x01 // stores initial subflow info even if it closed
-#define ENABLED				1
+#define ENABLED							1
 //-----------------------------------------------------
 
 // 1. Create socket
@@ -36,7 +37,7 @@
 
 void Error_Check(int input, const char *err_msg)
 {
-	if (input == ERROR)
+	if (input <= ERROR)
 	{	
 		perror(err_msg);
 		exit(ERROR);
@@ -58,6 +59,7 @@ void main(void)
 	double CPU_time;
 
 //-----------------------------------------------------
+	int mptcp_capable;
 	int val = MPTCP_INFO_FLAG_SAVE_MASTER; // info from inital subflow is stored even if it closed
 	int mptcp_enable = ENABLED;
 
@@ -83,7 +85,7 @@ void main(void)
 
 	// see 'man socket' for API and required headers
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	Error_Check(status, "socket: ");
+	Error_Check(socket_fd, "socket: ");
 
 	status = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option));
 	Error_Check(status, "setsockopt: REUSE(ADDR|PORT) ");
@@ -96,19 +98,19 @@ void main(void)
     	status = setsockopt(socket_fd, SOL_TCP, MPTCP_PATH_MANAGER, pathmanager, sizeof(pathmanager));
 	Error_Check(status, "setsockopt: MPTCP_PATH_MANAGER ");
 	
-	char *scheduler = "redundant"; // {default, roundrobin, redundant}
-	status = setsockopt(socket_fd, SOL_TCP, MPTCP_SCHEDULER, scheduler, sizeof(scheduler));
-	Error_Check(status, "setsockopt: MPTCP_SCHEDULER ");
+	//char *scheduler = "redundant"; // {default, roundrobin, redundant}
+	//status = setsockopt(socket_fd, SOL_TCP, MPTCP_SCHEDULER, scheduler, sizeof(scheduler));
+	//Error_Check(status, "setsockopt: MPTCP_SCHEDULER ");
 
 	status = setsockopt(socket_fd, SOL_TCP, MPTCP_INFO, &val, sizeof(val));
 	Error_Check(status, "setsockopt: MPTCP_INFO_FLAG ");
 
 	getsockopt(socket_fd, SOL_TCP, MPTCP_INFO, &minfo, (socklen_t *) sizeof(minfo));
-	getsockopt(socket_fd, SOL_SOCKET, MPTCP_INFO, &minfo, (socklen_t *) sizeof(minfo));
+	//getsockopt(socket_fd, SOL_SOCKET, MPTCP_INFO, &minfo, (socklen_t *) sizeof(minfo));
 
 	getsockopt(socket_fd, SOL_TCP, MPTCP_ENABLED, &mptcp_enable, (socklen_t *) sizeof(mptcp_enable));
 	printf("Is MPTCP_ENABLED: %d\n", mptcp_enable);
-	
+
 //-----------------------------------------------------
 
 	server.sin_family = AF_INET;
@@ -129,10 +131,16 @@ void main(void)
 	{
 		// Next, we accept the client connections from the queue and store its information
 		opened_socket = accept(socket_fd, (struct sockaddr *)&client, (socklen_t *)&len);
-		Error_Check(opened_socket, "accept: ");
+		Error_Check(opened_socket, "accept: ");		
 
-		
-			
+// Not sure which socket to use to get the MPTCP_Enable value
+		getsockopt(socket_fd, SOL_TCP, MPTCP_ENABLED, &mptcp_enable, (socklen_t *) sizeof(mptcp_enable));
+		printf("Is MPTCP_ENABLED: %d\n", mptcp_enable);
+
+		getsockopt(opened_socket, SOL_TCP, MPTCP_ENABLED, &mptcp_enable, (socklen_t *) sizeof(mptcp_enable));
+		printf("Is MPTCP_ENABLED: %d\n", mptcp_enable);	
+//
+
 		t_start = clock();
 		recv_bytes = recv(opened_socket, recv_buffer, BUFF_SIZE, 0);
 		sent_bytes = send(opened_socket, returned_msg, strlen(returned_msg), 0);
