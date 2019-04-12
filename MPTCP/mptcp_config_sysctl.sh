911564f49856a52f931c15fb865d276beabfc143
Tex Altstaetter <daltstaetter@gmail.com>
2019-04-12 18:47:03 -0500

    Reordered sysctl to reflect systctl -a order
    Signed-off-by: Tex Altstaetter <daltstaetter@gmail.com>

--- MPTCP/mptcp_config_sysctl.sh
+++ MPTCP/mptcp_config_sysctl.sh
@@ -1,1 +1,1 @@
-sudo sysctl -w net.mptcp.mptcp_checksum=0 net.mptcp.mptcp_scheduler=redundant net.mptcp.mptcp_enabled=1 net.mptcp.mptcp_path_manager=fullmesh net.mptcp.mptcp_debug=1
+sudo sysctl -w net.mptcp.mptcp_checksum=0 net.mptcp.mptcp_debug=1 net.mptcp.mptcp_enabled=1 net.mptcp.mptcp_path_manager=fullmesh net.mptcp.mptcp_scheduler=redundant 
